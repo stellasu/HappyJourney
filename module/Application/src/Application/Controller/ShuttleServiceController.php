e0c9c132a2f8faedf45f975cdc7066c6f2a2c92f
biyaosu <biyaosu@gmail.com>
2016-08-02 22:00:06 -0700

    Add shuttler service main description.

--- module/Application/src/Application/Controller/ShuttleServiceController.php
+++ module/Application/src/Application/Controller/ShuttleServiceController.php
@@ -4,13 +4,28 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\JsonModel;
 use Zend\View\Model\ViewModel;
+use Service\TextService;
 
 class ShuttleServiceController extends AbstractActionController {
 	
 	public function indexAction()
     {
     	$view = new ViewModel();
-    	return $view;
+		$textService = new TextService($this->serviceLocator);
+		try {
+			$textResults = $textService->getShuttleServiceMainDescription();
+		} catch (\Exception $e) {
+			error_log("error: ".$e->getMessage());
+		}
+		$results = null;
+		if($textResults != null){
+			$text = $textResults[0]->Text;
+			$results->text = $text;
+			$view->setVariables(array('status'=>0, 'results'=>$results));
+		}else{
+			$view->setVariables(array('status'=>1, 'results'=>null));
+		}
+		return $view;
     }
 	
 }
