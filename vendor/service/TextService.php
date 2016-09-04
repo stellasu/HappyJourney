7387fd34e4751732f6b56b041df8e35e61fae77f
biyaosu <biyaosu@gmail.com>
2016-09-03 20:46:59 -0700

    Fix getMostRecentText query error

--- vendor/service/TextService.php
+++ vendor/service/TextService.php
@@ -4,6 +4,7 @@ namespace Service;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\Db\ResultSet\ResultSet;
 use Zend\Db\Adapter\Driver\ResultInterface;
+use Zend\Db\Sql\Sql;
 
 class TextService {
 	
@@ -40,7 +41,7 @@ class TextService {
 	{
 		if(is_int($type) && $type>0){
 			try {
-				$query = "Select Text.* From Text Inner Join (Select Id, MAX(Version) Version From Text Where Type = ? Group By Id) S On Text.Version = S.Version Where Text.Type = ? And Deleted = 0";
+				$query = "Select Text.* from Text Inner Join (Select MAX(Version) Version, Type From Text Where Type = ?) S On Text.Version = S.Version where Text.Type = ? And Deleted = 0";
 				$sqlResult = $this->db->createStatement($query, array(intval($type), intval($type)))->execute();
 				$returnArray = null;
 				if ($sqlResult instanceof ResultInterface && $sqlResult->isQueryResult()) {
@@ -54,6 +55,44 @@ class TextService {
 				error_log("error: ".$e->getMessage());
 			}
 			return $returnArray;
+		}else{
+			return null;
+		}
+	}
+	
+	public function updateCustomizedTravelMainDescription(Array $data = null){
+		$data['Type'] = self::CT_MAIN_DESCRIPTION;
+		$data['Url'] = '/customizedtravel';
+		return $this->updateText($data);
+	}
+	
+	public function updateShuttleServiceMainDescription(Array $data = null){
+		$data['Type'] = self::SS_MAIN_DESCRIPTION;
+		$data['Url'] = '/shuttleservice';
+		return $this->updateText($data);
+	}
+	
+	/**
+	 * update text
+	 * @param array $data{Text, Version, Type, Url}
+	 */
+	public function updateText(Array $data = null)
+	{
+		if(isset($data['Text'])&&$data['Text']!=null 
+				&& isset($data['Version'])&&$data['Version']!=null 
+				&& isset($data['Type'])&&$data['Type']!=null){
+			try {
+				$sql = new Sql($this->db);
+				$insert = $sql->insert('Text');
+				$insert->values($data);
+				$statement = $sql->prepareStatementForSqlObject($insert);
+				$results = $statement->execute();
+				$lastInsertId = $this->db->getDriver()->getLastGeneratedValue();
+			} catch (\Exception $e) {
+				error_log("error: ".$e->getMessage());
+				return null;
+			}
+			return $lastInsertId;
 		}else{
 			return null;
 		}
